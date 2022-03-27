a71ae45ded4dfe3a43e789fbb2f6389e9d1472e5
Refat Tamanna Ringky <95683348+RefatRingky@users.noreply.github.com>
2022-03-28 02:45:26 +0600

    react bootstrap

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-
+import { Button, Spinner } from 'react-bootstrap';
 import './App.css';
-import CardGroup from './Component/CardGroup/CardGroup';
+import CardGroup2 from './Component/CardGroup2/CardGroup2';
 
 
 function App() {
   return (
     <div className="App">
-       <CardGroup></CardGroup>
+      <h1>Using React Bootstrap</h1>
+      <Button variant='danger'>My React Bootstrap Button</Button><br></br>
+      <Spinner animation="border" role="status">
+        <span className="visually-hidden">Loading...</span>
+      </Spinner>
+      <CardGroup2></CardGroup2>
     </div>
   );
 }
--- src/Component/CardGroup/CardGroup.js
+++ src/Component/CardGroup/CardGroup.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import Card from '../Card/Card';
 
 const CardGroup = () => {
+    const products = [
+        {id:1,name:'Laptop Pro' ,price:120000},
+        {id:2,name:'Laptop tro' ,price:130000},
+        {id:3,name:'Laptop fro' ,price:140000}
+    ]
+        
+    
     return (
         <div>
-            
+            <h1>This is My Class Group</h1>
+            <div className="card-group">
+               {
+                   products.map(product=> <Card key={product.id}></Card>)
+               }
+            </div>
         </div>
     );
 };
